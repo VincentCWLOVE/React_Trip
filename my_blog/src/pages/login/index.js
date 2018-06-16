e0f8e880ca1ef30700a0019c0061c5c9698964a4
vincent <438419787@qq.com>
2018-06-16 20:56:41 +0800

    update
    update

--- my_blog/src/pages/login/index.js
+++ my_blog/src/pages/login/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import logo from '../../images/brand.jpg'
+import './index.less';
 
 class Login extends Component {
   constructor() {
@@ -7,7 +9,29 @@ class Login extends Component {
 
   render() {
     return (
-      <h1>Login</h1>
+      <div className="login_container">
+      <form>
+        <div className="logo">
+          <img src={ logo }/>
+        </div>
+        <div className="email">
+          <input type="email"  id="exampleInputEmail1" placeholder="Email"/>
+        </div>
+        <div className="password">
+          <input type="password" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <div className="forget_password">
+          <span>忘记密码？</span>
+        </div>
+        <div className="login_btn">
+          <button>登陆</button>
+        </div>
+        <div className="register_acount">
+          <span>没有账号？注册</span>
+        </div>
+      </form>
+      </div>
+
     );
   }
 }
--- my_blog/src/pages/register/index.js
+++ my_blog/src/pages/register/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import logo from '../../images/brand.jpg';
+import './index.less';
 class Register extends Component {
   constructor() {
     super()
@@ -7,7 +8,28 @@ class Register extends Component {
 
   render() {
     return (
-      <h1>Register</h1>
+      <div className="register_container">
+      <form>
+        <div className="logo">
+          <img src={ logo }/>
+        </div>
+        <div className="email">
+          <input type="email"  id="exampleInputEmail1" placeholder="Email"/>
+        </div>
+        <div className="password">
+          <input type="password" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <div className="forget_password">
+          <span>忘记密码？</span>
+        </div>
+        <div className="login_btn">
+          <button>登陆</button>
+        </div>
+        <div className="register_acount">
+          <span>没有账号？注册</span>
+        </div>
+      </form>
+      </div>
     );
   }
 }
